8697a85d3d6885622a3e3c0a7a1215c7a3dd52d7
MarcPRich <45465675+MarcPRich@users.noreply.github.com>
2024-11-11 16:24:23 -0500

    initial commit

---
+++ app-js.js
@@ -0,0 +1,8 @@
+// Save this as app.js
+const GOOGLE_SCRIPT_URL = 'YOUR_GOOGLE_SCRIPT_URL_HERE';
+
+function App() {
+  // Copy the entire React component code from earlier here
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
